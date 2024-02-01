0d63d30f5031c35e0eeaf5133bb28d03e4b8f465
Thomas Carsello <tkcarsello@gmail.com>
2024-02-01 16:56:14 -0500

    Starting front end

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (<div>Login</div>)
+
+}
+
+export default Login
---
+++ client/src/pages/Register.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const Register = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+
+    return (<div>Register</div>)
+
+}
+
+export default Register
